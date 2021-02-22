8c4ea98664e4180d80a8099adba5497aa0aec400
Iván Trujillo <ivtrujillo12@gmail.com>
2021-02-22 16:59:06 +0000

    Change the sock for massively and added components

--- lab702/src/app/app.module.ts
+++ lab702/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FeaturedPostComponent } from './featured-post/featured-post.component';
+import { PostsComponent } from './posts/posts.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    NavBarComponent,
+    FeaturedPostComponent,
+    PostsComponent,
+    PaginationComponent,
+    FooterComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule
